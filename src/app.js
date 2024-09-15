fb3b80d495818f1e473b624b5aec480d2f80b237
MaxiZet <98966844+MaxiZet@users.noreply.github.com>
2024-09-15 22:34:47 +0000

    Primer Ajuste

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "Tinki Winki",
+    "Michael Jackson",
+    "Milei",
+    "Cristiano Ronaldo",
+    "Lizzy Tagliani"
+  ];
+  let action = ["se comio", "incendio", "aniquilo", "orino", "vomito"];
+  let what = [
+    "my computadora",
+    "mi casa",
+    "mi perro",
+    "mi integridad",
+    "mi fe en la humanidad"
+  ];
+  let when = [
+    "a la salida del estadio",
+    "durante la cena",
+    "cuando estaba estudiando",
+    "a las 2 de la mañana",
+    "el 3 de otoño"
+  ];
+
+  function getRandom(array) {
+    let randomNumber = Math.floor(Math.random() * array.length);
+    return array[randomNumber];
+  }
+
+  let excusa = `
+${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}
+`;
+
+  console.log(excusa);
+  document.getElementById("excuse").innerHTML = excusa;
 };
